cb9ee70dc6d6e26962b565106bb4c280f87a9a8d
Narayan <narayanband1356@gmail.com>
2023-05-07 21:13:30 +0530

    Fixed the misuse of `option` function

--- cmake/common-config.cmake
+++ cmake/common-config.cmake
@@ -1,5 +1,13 @@
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
+# Taken from https://github.com/SFML/SFML
+macro(set_option var default type docstring)
+    if(NOT DEFINED ${var})
+        set(${var} ${default})
+    endif()
+    set(${var} ${${var}} CACHE ${type} ${docstring} FORCE)
+endmacro()
+
 # Project name based on the given platform name
-option(PROJECT_TARGET_NAME "The postfix added to the project name" "generic")
+set_option(PROJECT_TARGET_NAME "generic" STRING "The postfix added to the project name")
